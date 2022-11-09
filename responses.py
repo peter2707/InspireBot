92136f306fce30992cad7ba970df40eee0f53310
Monkolsophearith Prum <62277365+peter2707@users.noreply.github.com>
2022-11-09 03:59:41 +0000

    bot now gets api data from zenquotes and sends response to user

--- responses.py
+++ responses.py
@@ -1,4 +1,13 @@
+import requests
+import json
 import random
+
+def get_quote():
+  response  = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" +json_data[0]['a']
+
+  return quote
 
 def handle_repsonse(message) -> str:
   p_message = message.lower()
@@ -11,3 +20,6 @@ def handle_repsonse(message) -> str:
 
   if p_message == '!help':
     return "`Please contact support for help.`"
+
+  if p_message == 'quote':
+    return get_quote()
